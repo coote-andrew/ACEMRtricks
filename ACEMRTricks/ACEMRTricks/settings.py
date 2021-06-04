67d7a624adcb556d1283f1d8ebad624ca3b6c490
A. Coote <accoote@Andrews-MBP-3.modem>
2021-06-04 19:22:29 +1000

    finished?

--- ACEMRTricks/ACEMRTricks/settings.py
+++ ACEMRTricks/ACEMRTricks/settings.py
@@ -142,3 +142,6 @@ STATICFILES_DIRS = (
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+import django_heroku
+django_heroku.settings(locals())
